2714c3b842202601129a9ddbd01da9bd2348fd62
seranomi <2bumchan@naver.com>
2025-01-13 17:12:12 +0900

    chore : EJS 템플릿 엔진 설치 및 기본 설정, 라우터 설정

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
-const express = require('express') // 서버 프레임워크
-const app = express() // 인스턴스화
+const express = require('express')
+const ejs = require('ejs')
+const app = express()
 const port = 3000
 
-// 라우터 함수
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
 app.get('/', (req, res) => {
-    console.log('Got a GET request from Client')
-    res.send('Got a response from Server')
-})// 브라우저에서는 get요청만 됨
-
-app.post('/', (req, res) => {
-    console.log('Got a POST request from Client')
-    res.send('Got a response from Server')
-})// curl -X POST localhost:3000/user
-
-app.put('/user', (req, res) => {
-    console.log('Got a PUT request from Client')
-    res.send('Got a response from Server')
+    res.render('index'); // .ejs생략가능
 })
 
-app.delete('/user', (req, res) => {
-    console.log('Got a DELETE request from Client')
-    res.send('Got a response from Server')
+app.get('/blog', (req, res) => {
+    res.render('blog');
 })
 
-// 
+app.get('/users', (req, res) => {
+    res.render('users');
+})
+
 app.listen(port, () => {
     console.log(`Node Legacy App listening on port ${port}`)
-}) // port넘버
-// 종료 ctrl + c
+})
