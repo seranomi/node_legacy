1f2568b69685fc1109b92477c14c55c5088b9b86
seranomi <2bumchan@naver.com>
2025-01-15 11:00:52 +0900

    feat : body-parser 설정하기

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const ejs = require('ejs')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse JSON
+app.use(bodyParser.json())
 // static file serving
 app.use(express.static(__dirname+'/public')) // __dirname 현재 경로
 
 app.get('/', (req, res) => {
     res.render('index'); // .ejs생략가능
+})
+
+app.post('/api/contact', (req, res) => {
+    const name = req.body.name;
+    const phone = req.body.phone;
+    const email = req.body.email;
+    const memo = req.body.memo;
+  
+    const data = `${name} ${phone} ${email} ${memo}`
+  
+    res.send(data);
 })
 
 app.get('/blog', (req, res) => {
@@ -18,6 +34,10 @@ app.get('/blog', (req, res) => {
 
 app.get('/users', (req, res) => {
     res.render('users');
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
 })
 
 app.listen(port, () => {
