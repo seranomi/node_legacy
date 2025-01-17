fb0c2413e733289425036cd48cf4afbddfe4fdd4
seranomi <2bumchan@naver.com>
2025-01-17 12:46:57 +0900

    refactor : Model 분리

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
-const mysql = require("mysql2");
-require("dotenv").config();
+
 const app = express();
 const port = 3000;
 
@@ -14,26 +13,7 @@ app.use(express.static(__dirname + "/public")); // __dirname 현재 경로
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse JSON
 app.use(bodyParser.json());
-// MySQL Connection Pool :
-// MySQL 커넥션을 사용 할 때는, 주로 커넥션 풀을 이용하여 관리하는 것이 권장된다.
-const connectionPool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PW,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  connectionLimit: 10, // 최대 연결 수 설정(필요시)
-  insecureAuth: true,
-});
-// MySQL connection check
-connectionPool.getConnection((err, connection) => {
-  if (err) {
-    console.error("MySQL 연결 중 에러 발생: ", err);
-  } else {
-    console.log("MySQL에 연결되었습니다.");
-    connection.release();
-  }
-});
+
 
 app.get("/", (req, res) => {
   const selectQuery = `select * from contact order by id desc`;
